04ee72098750a49cc84c421700e668380546ceb3
Jared Bada <official.jared.bada@gmail.com>
2024-11-23 22:02:22 +0300

    done

--- src/app/pages/clients/view/welfare.resolver.ts
+++ src/app/pages/clients/view/welfare.resolver.ts
@@ -1,13 +1,18 @@
 import { ResolveFn } from '@angular/router';
 import { inject } from '@angular/core';
 import { Welfare } from '../../../core/entities/welfare.entity';
-import { firstValueFrom } from 'rxjs';
+import { firstValueFrom, switchMap } from 'rxjs';
 import { AuthService } from '../../../core/services/auth.service';
 import { WelfaresService } from '../welfares/welfares.service';
 
 export const welfareResolver: ResolveFn<Welfare> = (route, state) => {
   const authService = inject(AuthService);
   const welfareService = inject(WelfaresService);
-
-  return firstValueFrom(authService.welfare);
+  return firstValueFrom(
+    authService.welfare.pipe(
+      switchMap((welfare) => {
+        return welfareService.getWelfareById(welfare.id!);
+      })
+    )
+  );
 };
